77c1450de4d03232b7ccacb3e3117dc048b59bac
claw501 <beenjammin501@gmail.com>
2025-04-22 17:05:33 -0500

    Add project files.

---
+++ InventoryItems.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingInventory
+{
+    public class Arrow : InventoryItem
+    {
+        public Arrow() : base(0.01, 0.05)
+        { 
+        }
+    }
+    public class Bow : InventoryItem
+    {
+        public Bow() : base(1, 4)
+        {
+        }
+    }
+    public class Rope : InventoryItem
+    {
+        public Rope() : base(1, 1.5)
+        {
+        }
+    }
+    public class Water : InventoryItem
+    {
+        public Water() : base(2, 3)
+        {
+        }
+    }
+    public class Food : InventoryItem
+    {
+        public Food() : base(1, 0.5)
+        {
+        }
+    }
+    public class Sword : InventoryItem
+    {
+        public Sword() : base(5, 3)
+        {
+        }
+    }
+}
---
+++ Pack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingInventory
+{
+    public class Pack
+    {
+        public Pack() 
+        {
+            int[] pack = new int[3];
+
+            GetItemAmount(pack);
+            GetItemWeight(pack);
+            GetItemVolume(pack);
+
+        }
+
+        private void GetItemAmount(int[] pack)
+        {
+            while (true)
+            {
+                Console.WriteLine("How many items will the pack be able to store? ");
+                string trypack = Console.ReadLine();
+                int.TryParse(trypack, out pack[0]);
+                continue;
+
+            }
+        }
+
+        private void GetItemWeight(int[] pack)
+        {
+            while (true)
+            {
+                Console.WriteLine("How much weight will the bag be able to carry? ");
+                string trypack = Console.ReadLine();
+                int.TryParse(trypack, out pack[1]);
+                continue;
+
+            }
+        }
+        private void GetItemVolume(int[] pack)
+        {
+            while (true)
+            {
+                Console.WriteLine("How much volume will the bag be able to carry? ");
+                string trypack = Console.ReadLine();
+                int.TryParse(trypack, out pack[2]);
+                continue;
+
+            }
+        }
+
+    }
+}
